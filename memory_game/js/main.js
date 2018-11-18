3a7f8e081c6e7ae127bd216745b39425622ca13b
Will <wsnjie@gmail.com>
2018-11-18 13:55:23 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and Running :)")
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
